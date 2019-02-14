a6bcbf176ad27833887318168df5e0e9ecae7812
Cleison C <30348651+Siltharin@users.noreply.github.com>
2019-02-14 16:08:14 -0200

    Update insertDE.js

--- insertDE.js
+++ insertDE.js
@@ -1,7 +1,7 @@
 <script runat="server">
 Platform.Load("core","1");
 
-var de = DataExtension.Init('APIPostmand');
+var de = DataExtension.Init('DataExtensionOfHell');
 de.Rows.Add({FirstName:'Luci',LastName:'fer',Email:'666'});
 de.Rows.Lookup
 
