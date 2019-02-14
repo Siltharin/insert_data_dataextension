005ead53ed1f8bcf4f8db1af21eac672fd5181f6
Cleison C <30348651+Siltharin@users.noreply.github.com>
2019-02-14 16:06:23 -0200

    insert a new file

---
+++ .js
@@ -0,0 +1,8 @@
+<script runat="server">
+Platform.Load("core","1");
+
+var de = DataExtension.Init('APIPostmand');
+de.Rows.Add({FirstName:'Luci',LastName:'fer',Email:'666'});
+de.Rows.Lookup
+
+</script>
